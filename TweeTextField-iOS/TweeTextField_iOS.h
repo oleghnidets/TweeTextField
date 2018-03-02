26bd85b8419258041a24a0a221c430b2d9da3262
Oleg Gnidets <oleg.oleksan@gmail.com>
2018-03-02 13:12:38 +0200

    Shared TweeTextField framework target.

---
+++ TweeTextField-iOS/TweeTextField_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TweeTextField_iOS.h
+//  TweeTextField-iOS
+//
+//  Created by Oleg Gnidets on 3/2/18.
+//  Copyright © 2018 Oleg Gnidets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TweeTextField_iOS.
+FOUNDATION_EXPORT double TweeTextField_iOSVersionNumber;
+
+//! Project version string for TweeTextField_iOS.
+FOUNDATION_EXPORT const unsigned char TweeTextField_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TweeTextField_iOS/PublicHeader.h>
+
+
