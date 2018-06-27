fb9acf3994fd383d415f515fc987f312a0975bd8
Oleg Hnidets <oleg.oleksan@gmail.com>
2018-06-27 19:21:06 +0300

    #13. Framework via Carthage ins't working.

---
+++ TweeTextField/TweeTextField.h
@@ -0,0 +1,19 @@
+//
+//  TweeTextField.h
+//  TweeTextField
+//
+//  Created by Oleg on 6/27/18.
+//  Copyright © 2018 Oleg Gnidets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TweeTextField.
+FOUNDATION_EXPORT double TweeTextFieldVersionNumber;
+
+//! Project version string for TweeTextField.
+FOUNDATION_EXPORT const unsigned char TweeTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TweeTextField/PublicHeader.h>
+
+
